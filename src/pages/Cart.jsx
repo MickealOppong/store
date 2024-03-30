09aac6317761fbf8620d9576f8974ccc4fccfd21
yaw oppong <opp.mic@gmail.com>
2024-03-30 19:53:30 +0100

    cdd

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -6,17 +6,26 @@ const Cart = () => {
   const user = useSelector((state) => state.userState.user);
   const itemsInCart = useSelector((state) => state.cartState.itemsInCart)
   const dispatch = useDispatch();
-
+  if (itemsInCart === 0) {
+    return <section className="px-8 lg:px-32 my-20">
+      <SectionTitle title='Your cart is empty' />
+    </section>
+  }
 
   return <div className="px-8 lg:px-32 my-20">
     <SectionTitle title='Shopping cart' />
     <div className="flex  flex-col md:flex-row md:justify-between gap-12">
       <CartItemList />
       <div>
+
         <CartTotal />
-        {
-          user ? <Link to='/checkout' className={`btn btn-primary mt-8 w-80 text-gray-100 uppercase ${itemsInCart > 0 ? 'flex' : 'hidden'}`}>proceed to checkout</Link> : <Link to="/login" className="btn btn-secondary mt-8 w-80 text-gray-100 uppercase">please login</Link>
-        }
+
+        <div>
+          {
+            user ? <Link to='/checkout' className={`btn btn-primary mt-8 w-80 text-gray-100 uppercase ${itemsInCart > 0 ? 'flex' : 'hidden'}`}>proceed to checkout</Link> : <Link to="/login" className="btn btn-secondary mt-8 w-80 text-gray-100 uppercase">please login</Link>
+          }
+        </div>
+
       </div>
     </div>
   </div>
